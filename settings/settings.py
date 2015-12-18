6cae5a5a0f38f03fbd469fbe852d89f6b695f007
Ivan Lukyanets <ivan@il-studio.ru>
2015-12-18 12:15:41 +0500

    update to django 1.8.7

--- settings/settings.py
+++ settings/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
-LANGUAGE_CODE = 'ru-TU'
+LANGUAGE_CODE = 'ru-RU'
 
 TIME_ZONE = 'Asia/Yekaterinburg'
 
