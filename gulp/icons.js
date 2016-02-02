74e7f3d2c6768dd1388b5b0ebd7d9282300a2d7f
Alex <fastex93@gmail.com>
2016-02-02 11:10:18 +0500

    fixed svg sprite task

--- gulp/icons.js
+++ gulp/icons.js
@@ -1,26 +1,29 @@
 import gulp from 'gulp';
-import svgSymbols from 'gulp-svg-symbols';
+import svgSprite from 'gulp-svg-sprite';
 import gulpif from 'gulp-if';
 import rename from 'gulp-rename';
 import plumber from 'gulp-plumber';
 import errorHandler from 'gulp-plumber-error-handler';
 import path from 'path';
 
+const svgSpriteConfig = {
+	mode: {
+		symbol: {
+			render: {
+				styl: {dest: 'svg-size.styl'}
+			}
+		}
+	}
+};
+
 gulp.task('icons', () => (
 	gulp.src(['**/*.svg'], {
 			cwd: 'frontend/images/icons'
 		})
 		.pipe(plumber({errorHandler: errorHandler('Error in \'icons\' task')}))
-		.pipe(svgSymbols({
-			title: false,
-			id: 'icon_%f',
-			className: '%f',
-			templates: [
-				path.join(__dirname, '../node_modules/stylus-svg-size-template/svg-size.styl'),
-				'default-svg'
-			]
-		}))
-		.pipe(gulpif(/\.styl$/, gulp.dest('frontend/styles/helpers')))
+		.pipe(svgSprite(svgSpriteConfig))
 		.pipe(gulpif(/\.svg$/, rename('icon.svg')))
+		.pipe(gulpif(/\.styl$/, rename('svg-size.styl')))
+		.pipe(gulpif(/\.styl$/, gulp.dest('frontend/styles/helpers')))
 		.pipe(gulpif(/\.svg$/, gulp.dest('static/images')))
 ));
