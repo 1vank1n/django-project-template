eda54e09299b5161d5c80b56e32c94c4aa67b0f3
Ivan Lukyanets <lukyanets.ivan@gmail.com>
2023-12-13 12:44:27 +0300

    add ru_plural to core_tags

--- applications/core/templatetags/core_tags.py
+++ applications/core/templatetags/core_tags.py
@@ -15,3 +15,29 @@ def published(value):
 @register.filter
 def to_json(value):
     return json.dumps(value)
+
+
+@register.filter
+def ru_plural(value, variants):
+    """
+    Множественные окончания для русских слов.
+
+    Пример:
+        1 подписчик
+        3 подписчика
+        20 подписчиков
+
+        {{ total_followers|ru_plural:'подписчик,подписчика,подписчиков' }}
+    """
+
+    variants = variants.split(',')
+    value = abs(int(value))
+
+    if value % 10 == 1 and value % 100 != 11:
+        variant = 0
+    elif value % 10 >= 2 and value % 10 <= 4 and (value % 100 < 10 or value % 100 >= 20):
+        variant = 1
+    else:
+        variant = 2
+
+    return variants[variant]
