122a435fb1b7d28796a84f2b10a5e3bd852c9097
Ivan Lukyanets <lukyanets.ivan@gmail.com>
2025-08-08 12:48:08 +0300

    Wrap debug middleware in DEBUG conditional

--- settings/components/debug.py
+++ settings/components/debug.py
@@ -7,10 +7,11 @@ if settings.DEBUG:
 
 # django-querycount
 # django-debug-toolbar
-settings.MIDDLEWARE += [
-    'querycount.middleware.QueryCountMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
-]
+if settings.DEBUG:
+    settings.MIDDLEWARE += [
+        'querycount.middleware.QueryCountMiddleware',
+        'debug_toolbar.middleware.DebugToolbarMiddleware',
+    ]
 
 # django-debug-toolbar
 INTERNAL_IPS = [
